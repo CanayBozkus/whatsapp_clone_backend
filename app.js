a025308762fee5aa6283d53bbeedb8fe7a8f6705
canaybozkus <bozkuscanay@gmail.com>
2021-06-02 16:25:36 +0300

    refactor: socket.onConnectionHandler() method

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken')
 
 const userAPIRoutes = require('./user_api/user_route')
 
@@ -30,28 +29,8 @@ mongoose
         const server = app.listen(port, () => {
             console.log('Listening port 3000')
         })
-        const io = require('./socket').init(server)
+        const socket = require('./socket')
+        const io = socket.init(server)
 
-        io.on('connection', socket => {
-
-            const authorizationHeader = socket.handshake.headers.authorization
-
-            if(!authorizationHeader){
-                io.sockets.connected[socket.id].disconnect();
-                console.log('disconnected');
-                return;
-            }
-
-            const token = authorizationHeader.split(' ')[1];
-
-            try{
-                jwt.verify(token, process.env.JWT_SECRET_KEY)
-            }
-
-            catch(e){
-                console.log('disconnected');
-                return io.sockets.connected[socket.id].disconnect();
-            }
-            console.log(`Client connected ${socket.id}`)
-        })
+        io.on('connection', socket.connectionHandler)
     })
--- socket.js
+++ socket.js
@@ -1,3 +1,5 @@
+const connections = require('./cache')
+
 let io
 
 module.exports = {
@@ -10,5 +12,30 @@ module.exports = {
             throw new Error('IO not initialized')
         }
         return io
-    }
+    },
+    connectionHandler: (socket) => {
+        const jwt = require('jsonwebtoken')
+
+        const authorizationHeader = socket.handshake.headers.authorization
+
+        if(!authorizationHeader){
+            io.sockets.connected[socket.id].disconnect();
+            console.log('disconnected');
+            return;
+        }
+
+        const token = authorizationHeader.split(' ')[1];
+        let decodedToken;
+        try{
+            decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        }
+
+        catch(e){
+            console.log('disconnected');
+            return io.sockets.connected[socket.id].disconnect();
+        }
+
+        connections[decodedToken['phoneNumber']] = socket.id
+        console.log(`Client connected ${socket.id}`)
+    },
 }
