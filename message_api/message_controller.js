faea446ecba49a3216104cb7ea06b86f07e62028
canaybozkus <bozkuscanay@gmail.com>
2021-06-03 23:58:43 +0300

    send message route validations and controller

--- message_api/message_controller.js
+++ message_api/message_controller.js
@@ -1,4 +1,40 @@
+const { validationResult } = require('express-validator')
+const User = require('../models/user')
+const connections = require('../cache')
+const socket = require('../socket')
+
 exports.sendMessage = async (req, res, next) => {
+    const validationErrors = validationResult(req)
+
+    if(!validationErrors.isEmpty()){
+        return res.json({
+            success: false,
+            message: validationErrors
+        })
+    }
+
+    const message = req.body.message
+    const to = req.body.to
+    const roomId = req.body.roomId
+    const sendTimeString = req.body.sendTime
+
+    const toUser = await User.findOne({phoneNumber: to})
+
+    if(!toUser){
+        return res.json({
+            success: false,
+        })
+    }
+
+    const socketId = connections[to]
+
+    socket.getIO().to(socketId).emit('message', {
+        message,
+        to,
+        roomId,
+        sendTime: sendTimeString
+    })
+
     res.json({
         success: true
     })
--- message_api/message_route.js
+++ message_api/message_route.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const { body } = require('express-validator');
 
 const loginRequired = require('../middleware/login_required')
 const controller = require('./message_controller')
+const Constant = require('../user_api/constant')
 
 const router = express.Router()
 
 router.post(
     '/send-message',
     loginRequired,
+    body('message').exists().isString().isLength({min: 1}),
+    body('to')
+        .notEmpty()
+        .withMessage('Phone number is required')
+        .replace(Constant.clearPhoneNumberRegex, '')
+        .isLength({min: 10})
+        .withMessage('Invalid phone number'),
+    body('roomId').exists().isString().isLength({min: 1}),
+    body('sendTime').exists().isISO8601(),
     controller.sendMessage
 )
 
